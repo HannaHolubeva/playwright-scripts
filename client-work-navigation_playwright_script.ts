492414d969cecd8846b1648012bb0997ed4b8102
HannaHolubeva <44155636+HannaHolubeva@users.noreply.github.com>
2026-02-11 09:28:51 +0300

    Create client-work-navigation_playwright_script.ts

---
+++ client-work-navigation_playwright_script.ts
@@ -0,0 +1,8 @@
+import { test, expect } from '@playwright/test';
+
+// Test: Client Work Navigation
+// This file will contain a Playwright test that navigates from the EPAM header to the Client Work page.
+
+test('Client Work Navigation - header to client work page', async ({ page }) => {
+  // TEST_BODY_PLACEHOLDER
+});
